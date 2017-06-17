b6d8ce1c80f53affbe5523d02a209272d5804789
Danny Ryan <dannyjryan@gmail.com>
2017-06-16 21:18:05 -0500

    update intialization tests

--- test/TestBlockStory.sol
+++ test/TestBlockStory.sol
@@ -5,14 +5,29 @@ import "truffle/DeployedAddresses.sol";
 import "../contracts/BlockStory.sol";
 
 contract TestBlockStory {
-  function testValidInitialization() {
-    BlockStory story = BlockStory(DeployedAddresses.BlockStory());
+    uint numLines;
+    uint lineLength;
 
-    uint expected = 0;
+    function beforeEach() {
+        numLines = 10;
+        lineLength = 50;
+    }
+ 
+    function testValidDeployedContract() {
+        BlockStory story = BlockStory(DeployedAddresses.BlockStory());
 
-    Assert.equal(story.index(), 0, "Index should be initialized to 0");
-    Assert.isAbove(story.lineLength(), 0, "lineLength is required to be greater than 0");
-    Assert.isAbove(story.numLines(), 0, "numLines is required to be greater than 0");
-    //Assert.equal(story.submissions().length, 1, "There should be 1 submission.");
-  }
+        Assert.equal(story.index(), 0, "Index should be initialized to 0");
+        Assert.isAbove(story.lineLength(), 0, "lineLength should be greater than 0");
+        Assert.isAbove(story.numLines(), 0, "numLines should be greater than 0");
+        //Assert.equal(story.submissions().length, 1, "There should be 1 submission.");
+    }
+
+    function testValidInitialization() {
+        BlockStory story = new BlockStory(numLines, lineLength);
+
+        Assert.equal(story.index(), 0, "Index should be initialized to 0");
+        Assert.equal(story.lineLength(), lineLength, "lineLength should be initialized to val");
+        Assert.equal(story.numLines(), numLines, "numLines should be initialized to val");
+        //Assert.equal(story.submissions().length, 1, "There should be 1 submission.");
+    }
 }
